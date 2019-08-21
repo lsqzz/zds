15ad183a22463a41b6b6db135dcb8f2ab0f823c9
lsqzz <2364788985@qq.com>
2019-08-21 09:40:27 +0800

    发现页面完成一半

---
+++ src/store/modules/pingpai.js
@@ -0,0 +1,80 @@
+//引入axios实例的模块
+import request from '../../utils/request'
+import { type } from 'os'
+
+export default {
+  namespaced: true,
+
+  state() {
+    return {
+      //品牌页总数据
+      data: [],
+      //品牌页内容顶部图片
+      img: '',
+      //品牌内容 中间  珠宝图片
+      zbImg: []
+    }
+  },
+
+  geeters: {},
+
+  mutations: {
+    setData(state, payload) {
+      state.data = payload
+    },
+
+    //设置品牌内容 顶部图片
+    setImg(state, payload) {
+      state.img = payload
+    },
+
+    //设置品牌内容 中间 珠宝图片
+    setZbImg(state, payload) {
+      state.zbImg = payload
+    }
+  },
+
+  actions: {
+    //获取内容顶部 图片 数据
+    getImg({ commit }) {
+      request
+        .get(
+          '/ishop/web/?app_act=api/&method=article.get.pinpai.banner&sid=8sgb172tb1psut1tr5fnc4ttb0&sign=b264f15b0fdca0e0a203c3556c8d8da6'
+        )
+        .then(res => {
+          if (res.status === '1') {
+            console.log(res.data[0].article_img)
+            commit('setImg', res.data[0].article_img)
+          }
+        })
+    },
+
+    //获取珠宝图片
+    getZbImg({ commit }) {
+      request
+        .get(
+          '/ishop/web/?app_act=api/&method=article.get.category&sid=8sgb172tb1psut1tr5fnc4ttb0&sign=b264f15b0fdca0e0a203c3556c8d8da6'
+        )
+        .then(res => {
+          if (res.status === '1') {
+            console.log(res.data)
+            commit('setZbImg', res.data)
+          }
+        })
+    },
+
+    //获取的 精选推荐 数据
+    getAjax({ commit }) {
+      request
+        .get(
+          '/ishop/web/?app_act=api/&method=article.get.select&sid=8sgb172tb1psut1tr5fnc4ttb0&sign=b264f15b0fdca0e0a203c3556c8d8da6'
+        )
+        .then(res => {
+          if (res.status === '1') {
+            console.log(res.data)
+            commit('setData', res.data)
+          }
+        })
+    }
+  }
+}
--- src/views/Home/pingpai.vue
+++ src/views/Home/pingpai.vue
@@ -1,5 +1,114 @@
 <template>
   <div class="page-pingpai">
-    <h1>我是品牌页</h1>
+    <header class="pp_header">
+      <span>发现</span>
+      <i>消息</i>
+    </header>
+    <div class="pp_comtent">
+      <div class="nav">
+        <img :src="img" alt />
+      </div>
+
+      <ul class="center">
+        <li v-for="item in zbImg" :key="item.cat_id">
+          <img :src="item.img" alt />
+          <span>{{item.cat_name}}</span>
+          <p>{{item.en_name}}</p>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
+
+<script>
+import { mapState, mapGetters, mapMutations, mapActions } from 'vuex'
+
+export default {
+  name: 'pingpai',
+
+  data() {
+    return {}
+  },
+
+  computed: {
+    ...mapState('pingpai', ['data', 'img', 'zbImg'])
+  },
+
+  methods: {
+    ...mapActions('pingpai', ['getAjax', 'getImg', 'getZbImg'])
+  },
+
+  created() {
+    this.getAjax()
+    this.getImg()
+    this.getZbImg()
+  }
+}
+</script>
+
+<style lang="scss" >
+.page-pingpai {
+  width: 100%;
+  height: 100%;
+
+  .pp_header {
+    width: 100%;
+    height: 78px;
+    position: fixed;
+    background: white;
+    top: 0;
+    left: 0;
+    z-index: 9999;
+    span {
+      line-height: 78px;
+      display: inline-block;
+      padding: 0 40%;
+    }
+  }
+
+  .pp_comtent {
+    padding-top: 78px;
+
+    .nav {
+      width: 100%;
+      height: 280px;
+      img {
+        width: 100%;
+      }
+    }
+
+    .center {
+      width: 100%;
+      height: 195px;
+      li {
+        float: left;
+        height: 158px;
+        width: 115px;
+        padding: 0 5px;
+        position: relative;
+        img {
+          width: 100%;
+        }
+        span {
+          position: absolute;
+          left: 0;
+          top: 90px;
+          display: block;
+          text-align: center;
+          width: 100%;
+          font-size: 14px;
+        }
+        p {
+          position: absolute;
+          left: 0;
+          top: 110px;
+          text-align: center;
+          width: 100%;
+          font-size: 12px;
+        }
+      }
+    }
+  }
+}
+</style>
+
